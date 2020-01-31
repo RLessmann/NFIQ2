b2031a3263baba70d5db9963c22368a166b0dae4
ralph lessmann <ralph.lessmann@crossmatch.com>
2020-01-31 14:49:13 +0100

    add submodule NFIQ2 training

--- cmake/astyle.cmake
+++ cmake/astyle.cmake
@@ -4,9 +4,11 @@ if( NOT ANDROID AND NOT "${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
   # Error	C1061	compiler limit: blocks nested too deeply	astyle	D:\Devel\ISO\NFIQ2-official\NFIQ2-rlessmann\astyle\src\astyle_main.cpp	3702	
   add_subdirectory("${ROOT_PATH}/astyle/" "${BUILD_PATH}/astyle/")
   # add project folders to be "astyled"
-  file(GLOB ASTYLE_FILES "${ROOT_PATH}/NFIQ2/NFIQ2Api/*.cpp" "${ROOT_PATH}/NFIQ2/NFIQ2Api/*.h" ) 
+  file(GLOB ASTYLE_NFIQ2_ALGORITHM "${ROOT_PATH}/NFIQ2/NFIQ2Api/*.cpp" "${ROOT_PATH}/NFIQ2/NFIQ2Api/*.h" ) 
+  file(GLOB ASTYLE_NFIQ2_API "${ROOT_PATH}/NFIQ2/NFIQ2Api/*.cpp" "${ROOT_PATH}/NFIQ2/NFIQ2Api/*.h" ) 
+  file(GLOB ASTYLE_NFIQ2_TRAINING "${ROOT_PATH}/NFIQ2/NFIQ2Api/*.cpp" "${ROOT_PATH}/NFIQ2/NFIQ2Api/*.h" ) 
   add_custom_target( format
-    COMMAND ${ASTYLE_EXE} --options=${ROOT_PATH}/astyle.cfg ${ASTYLE_FILES} 
+    COMMAND ${ASTYLE_EXE} --options=${ROOT_PATH}/astyle.cfg ${ASTYLE_NFIQ2_ALGORITHM} ${ASTYLE_NFIQ2_API} ${ASTYLE_NFIQ2_TRAINING}
     DEPENDS astyle
   )
 else()
